1a6813ad7cc13b0c4c4a27faf7045d4b6c0b821b
baha <tytenov99@gmail.com>
2020-11-29 23:49:58 +0600

    done movie list need to fix rating bar

---
+++ app/src/main/java/com/baha/androidfundamental/FragmentMoviesDetails.kt
@@ -0,0 +1,11 @@
+package com.baha.androidfundamental
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails :Fragment(R.layout.fragment_movies_details){
+
+
+
+}
--- app/src/main/java/com/baha/androidfundamental/MainActivity.kt
+++ app/src/main/java/com/baha/androidfundamental/MainActivity.kt
@@ -2,10 +2,13 @@ package com.baha.androidfundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.fragment_movies_list)
     }
+
+    fun onclick(view: View) {}
 }
